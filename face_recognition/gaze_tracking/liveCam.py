66b7402ec7d3250185dee81a82a89438b273e5e9
cutiepazzipozzi <sk000801@naver.com>
2023-04-26 16:49:34 +0900

    feat: 얼굴 인식 세 가지 코드 합치기-4

---
+++ face_recognition/gaze_tracking/liveCam.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+@app.route('/')
+def index():
+    return render_template('liveCam.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', 8080, debug=True)
